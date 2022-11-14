19d6311d03b92134876069f30d6e9e3807605613
shinyehwan <jghisjl007@naver.com>
2022-11-14 18:48:11 +0900

    잭슨 라이브러리로 자바 객체를 json으로 변경

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,11 +2,25 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.ll.exam.article.dto.ArticleDto;
+
+import util.Ut;
+
 public class AppTest {
 	@Test
 	void 테스트_assertThat() {
 		int rs = 10 + 20;
 		assertThat(rs).isEqualTo(30);
+	}
+
+	@Test
+	void 실험_ObjectMapper() {
+		ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
+		String jsonStr = Ut.json.toJsonStr(articleDto, "");
+		assertThat(jsonStr).isNotBlank();
+		assertThat(jsonStr).isEqualTo("""
+				{"id":1,"title":"제목","body":"내용"}
+			""".trim());
 	}
 }
 
